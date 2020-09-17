54836cd437615410a46aed1248e7d12661a87c6b
devaraujstein <andreearaujo98@gmail.com>
2020-09-17 19:49:24 -0300

    six tests done sequentially

---
+++ src/database/migrations/1600374357952-TransactionsCategoriesFK.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner, TableForeignKey } from 'typeorm';
+
+export default class TransactionsCategoriesFK1600374357952
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createForeignKey(
+      'transactions',
+      new TableForeignKey({
+        name: 'TransactionCategory',
+        columnNames: ['category_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'categories',
+        onDelete: 'SET NULL',
+        onUpdate: 'CASCADE',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey('transactions', 'TransactionCategory');
+
+    await queryRunner.dropColumn('transactions', 'category_id');
+  }
+}
